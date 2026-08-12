ff63a1dd97aefa3dee50ac41dbb197ce7aa0177f
Craig Haywood <admin@brisbanesilicon.com.au>
2026-08-12 10:19:40 +1000

    update1: add files

---
+++ Update1/application-skeleton.lua
@@ -0,0 +1,23 @@
+--[[
+        Skeleton user Application-Layer program
+--]]
+
+program_name = "<NONE>"
+
+
+-- Program dependencies
+
+
+-- Program configuration
+
+
+-- Main program begin
+
+print("\n -------- Program begin: "..program_name.." --------\n")
+msleep(500)
+
+
+
+-- Main program end
+
+print("\n -------- Program end: "..program_name.." --------\n")
---
+++ Update1/driver-skeleton.c
@@ -0,0 +1,83 @@
+#define LUA_LIB
+
+#include "lprefix.h"
+
+#include <limits.h>
+#include <stddef.h>
+#include <string.h>
+
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+
+
+// Skeleton user Driver-Layer program
+
+
+/* ------------------ Users Lua API ----------------------  */
+
+    // NOTE: User to add any custom API
+    // functions here.
+
+    // NOTE: Ensure that all API functions are
+    // included in the 'User API Registry' below,
+    // in order for them to be callable from the
+    // Lua appliction layer.
+
+
+
+
+
+/* ------------------ User API Registry ----------------------------  */
+
+static const luaL_Reg user_api_registry[] = {
+    // NOTE: ensure you add any new custom API
+    // functions to this array in order to ensure
+    // they can be imported from the Lua Interpreter.
+
+    // NOTE: the { "Function Name", Function Pointer } entries
+    // below are the 'Example Lua API' functions that are
+    // defined previously.
+
+
+
+    {NULL, NULL}
+};
+
+
+
+/* ------------------ Boilerplate Library Code ----------------------  */
+
+static const luaL_Reg userlib_default[] = {
+    {NULL, NULL}
+};
+
+/*
+** Open user library
+*/
+LUAMOD_API int luaopen_user (lua_State *L) {
+    luaL_newlib(L, userlib_default);
+    return 1;
+}
+
+LUAMOD_API int luaimportfunc_user (lua_State *L, const char *fname) {
+    uint32_t i;
+
+    i = 0;
+    while(1) {
+        if (!user_api_registry[i].name) {
+            return 1;
+        }
+
+        if (strcmp(fname, user_api_registry[i].name) == 0) {
+            lua_getglobal(L, LUA_USERLIBNAME);
+            luaL_setfunc(L, &user_api_registry[i], 0);
+
+            break;
+        }
+
+        i++;
+    }
+
+    return 0;
+}
